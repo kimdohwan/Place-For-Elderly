f71f827505f66c98aa22245a9e324643537d1b9c
kimdohwan <dosio0102@gmail.com>
2019-05-10 21:48:58 +0900

    [ADD] 로그인 기능 추가

---
+++ app/members/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.contrib.auth import get_user_model, password_validation
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()
+
+
+class SignupForm(UserCreationForm):
+    username = forms.CharField(
+        label="아이디",
+        max_length=100,
+        required=True
+    )
+    password1 = forms.CharField(
+        label="비밀번호",
+        strip=False,
+        widget=forms.PasswordInput,
+        help_text="8자리 이상",
+    )
+    password2 = forms.CharField(
+        label="비밀번호 확인",
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text="이전에 입력한 비밀번호와 동일한 비밀번호를 입력",
+    )
+
+    error_messages = {
+        'password_mismatch': "비밀번호 불일치",
+    }
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'password1',
+            'password2',
+        )
--- app/members/views.py
+++ app/members/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import SignupForm
+
+
+class SignupView(CreateView):
+    template_name = 'members/signup.html'
+    form_class = SignupForm
+    success_url = reverse_lazy('index')
+
+# def signup_view(request):
+#     if request.method == 'POST':
+#         form = SignupForm(request.POST)
+#     else:
+#         form = SignupForm()
+#
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, 'members/signup.html', context)
+
