2ec7a64c52ac8253773b6a5c42510e273e720737
kimdohwan <dosio0102@gmail.com>
2019-05-10 21:46:49 +0900

    [ADD] members app 추가

--- app/config/urls.py
+++ app/config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('facilities.urls')),
     path('admin/', admin.site.urls),
+    path('members/', include('members.urls'))
 ]
