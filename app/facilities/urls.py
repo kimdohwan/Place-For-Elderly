c847a709576d7f7be5b0c7ae3d926c3067e777e3
kimdohwan <dosio0102@gmail.com>
2019-05-10 19:33:47 +0900

    [FIX] index, detail, list, search 분리

--- app/facilities/urls.py
+++ app/facilities/urls.py
@@ -1,8 +1,11 @@
+from django.conf.urls import url
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
+    path('', views.index_view, name='index'),
     path('list/', views.FacilityListView.as_view(), name='facility-list'),
-    path('search/', views.search_view, name='search')
+    path('detail/<pk>', views.FacilityDetailView.as_view(), name='facility-detail'),
+    path('search/', views.search_view, name='search'),
 ]
