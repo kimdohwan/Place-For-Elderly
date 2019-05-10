a6c6c0e649b97ede52afb4135230ba2c8e2ed148
kimdohwan <dosio0102@gmail.com>
2019-05-10 19:34:39 +0900

    [FIX] detail view 추가, search view 검색 옵션 기능 추가

--- app/facilities/views.py
+++ app/facilities/views.py
@@ -1,8 +1,8 @@
 from django.db.models import Q
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from facilities.models import Facility
 
@@ -10,18 +10,38 @@ from facilities.models import Facility
 class FacilityListView(ListView):
     model = Facility
     # 모두 Default 설정 값과 동일
-    template_name = 'facilities/facility_list'
+    template_name = 'facilities/facility_list.html'
     context_object_name = 'facility_list'
     queryset = Facility.objects.all()
 
 
-def search_view(request):
-    qs = None
-    q = request.GET.get('q', '')
-    if q:
-        qs = Facility.objects.filter(Q(REFINE_LOTNO_ADDR__icontains=q) | Q(REFINE_ROADNM_ADDR__icontains=q)).order_by('SIGUN_CD')  # 시/군 필터링
+class FacilityDetailView(DetailView):
+    model = Facility
+    # 모두 Default 설정 값과 동일
+    template_name = 'facilities/facility_detail.html'
+    context_object_name = 'facility'
 
-    return render(request, 'facilities/search_view.html', {
-        'facility_list': qs,
-        'q': q,
-    })
+
+def index_view(request):
+    return redirect('facility-list')
+
+
+def search_view(request):
+    query_set = Facility.objects.none()
+
+    search_text = request.GET.get('search_text', None)
+    search_option = request.GET.get('search_option', 'address')
+    if search_option == 'facility_address':
+        query_set = Facility.objects.filter(
+            Q(REFINE_LOTNO_ADDR__icontains=search_text) |
+            Q(REFINE_ROADNM_ADDR__icontains=search_text)).order_by(
+            'SIGUN_CD'
+        )
+    elif search_option == 'facility_name':
+        query_set = Facility.objects.filter(BIZPLC_NM__icontains=search_text)
+
+    context = {
+        'facility_list': query_set,
+        'search_text': search_text,
+    }
+    return render(request, 'facilities/facility_list.html', context)
