0813242e2a9766a20ba76c41125cbf6adf5f6409
kimdohwan <dosio0102@gmail.com>
2019-05-12 21:48:10 +0900

    [ADD] LikeFacility 모델 추가

--- app/members/models.py
+++ app/members/models.py
@@ -1,13 +1,33 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-Facility = 'facilities.Facility'
+from facilities.models import Facility
 
+
+# - ForeignKey 작성 시 사용해 줄수 있는 model 경로
+# Facility = 'facilities.Facility'
 
 class User(AbstractUser):
-    likes = models.ForeignKey(
+
+    @property
+    def facility_list(self):
+        facility_list = []
+        # like_queryset = self.likefacility_set.prefetch_related()
+        like_queryset = self.likefacility_set.all()
+
+        for like_obj in like_queryset:
+            facility_obj = like_obj.facility
+            facility_list.append(facility_obj)
+
+        return facility_list
+
+
+class LikeFacility(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    facility = models.ForeignKey(
         Facility,
         on_delete=models.CASCADE,
-        related_name='facilities',
-        null=True,
     )
